76ad34eb0b8a8a7b80547c29acfdabcd8f7a81d8
Ankit Agarwal <agrawal.coolankit@gmail.com>
2021-05-08 19:23:14 +0530

    Add plugin to convert pdf to audiobook

---
+++ pdf_to_audiobook.py
@@ -0,0 +1,15 @@
+# Conver a pdf to an audiobook
+
+import pyttsx3
+import PyPDF2
+
+book = open('test.pdf', 'rb')
+pdfReader = PyPDF2.PdfFileReader(book)
+pages = pdfReader.numPages
+
+speaker = pyttsx3.init()
+for num in range(0, pages):
+    page = pdfReader.getPage(num)
+    text = page.extractText()
+    speaker.say(text)
+    speaker.runAndWait()
